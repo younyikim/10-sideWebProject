22c9c59bf70ebb8b0ebd0afba677e54a3d4261b1
younyikim <rlakuku1221@gmail.com>
2021-06-21 21:47:42 +0900

    feat:html 레이아웃(소개,skill, profile) 변경, modal 추가

--- 1.MenuSlider_Modal/script.js
+++ 1.MenuSlider_Modal/script.js
@@ -1,6 +1,28 @@
 let btnSideBarMenu = document.getElementById("sidebar-menu-btn");
 btnSideBarMenu.addEventListener("click", openSideBarMenu);
 
+let modal = document.querySelector(".showcase-modal");
+let modalClose = document.querySelector(".showcase-modal-close");
+let btnPortpolio = document.getElementById("showcase-portfolio-btn");
+
+//'My Project' 버튼을 클릭시, modal을 보여준다.
+btnPortpolio.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+
+// modal의 X(닫기 버튼)을 클릭 시, modal을 닫는다.
+modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// 사용자가 프로젝트 포트폴리오 modal 외부를 클릭 시, modal을 닫는다.
+window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+})
+
+
 let isSideBarCliked = true;
 function openSideBarMenu() {
     if (isSideBarCliked) {
