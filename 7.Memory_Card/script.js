5af59da0a4313493a710f2cf44e22391ff1e81e1
younyikim <rlakuku1221@gmail.com>
2021-08-06 17:48:25 +0900

    memory card : add UI navigation and card add container

---
+++ 7.Memory_Card/script.js
@@ -0,0 +1,8 @@
+/*
+ * Create flip cards using CSS
+    Create "Add new card" overlay with form
+    Display question cards and flip for answer
+    View prev and next cards
+    Add new cards to local storage
+    Clear all cards from local storage
+ */
