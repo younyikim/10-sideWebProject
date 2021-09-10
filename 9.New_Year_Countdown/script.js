5d8acb763891bdd4f0b16b0cb665c7b79fcb5607
younyikim <rlakuku1221@gmail.com>
2021-09-10 18:30:12 +0900

    New Year Countdown : JS 추가, 완료

---
+++ 9.New_Year_Countdown/script.js
@@ -0,0 +1,30 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const counterContainer = document.querySelector(".counter-container");
+
+const currentYear = new Date().getFullYear();
+const newYear = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// 내년 날짜 표시
+const nextYear = document.getElementById("nextYear");
+nextYear.innerHTML = newYear.getFullYear();
+
+function newYearCountdown(intervalId) {
+    const currentDate = new Date();
+    const time = newYear - currentDate;
+
+    const day = Math.floor(time / 1000 / 60 / 60 / 24);
+    const hour = Math.floor(time / 1000 / 60 / 60) % 24;
+    const min = Math.floor(time / 1000 / 60) % 60;
+    const sec = Math.floor(time / 1000) % 60;
+
+    days.innerHTML = day;
+    hours.innerHTML = hour > 10 ? hour : '0' + hour;
+    minutes.innerHTML = min > 10 ? min : '0' + min;
+    seconds.innerHTML = sec > 10 ? sec : '0' + sec;
+}
+
+// 1초마다 countdown 함수 실행
+setInterval(newYearCountdown, 1000);
